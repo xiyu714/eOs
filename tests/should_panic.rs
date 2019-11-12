e13508c99d1dccc2a03e07d6de9a139eb35417b2
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-11-12 18:48:12 +0800

    添加should_panic测试

---
+++ tests/should_panic.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use eOs::{QemuExitCode, exit_qemu, serial_println};
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+        serial_println!("[test did not panic]");
+        exit_qemu(QemuExitCode::Failed);
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+use eOs::serial_print;
+
+#[test_case]
+fn should_fail() {
+    serial_print!("should_fail... ");
+    assert_eq!(0, 1);
+}
+
