955f9b8393017a7d644cea8a15927dc7dfe64aba
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-11-12 15:23:55 +0800

    使用自定义测试框架

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 #![no_main] // 禁用所有Rust层级的入口点
 #![allow(non_snake_case)]
 #![feature(non_ascii_idents)]
+#![feature(custom_test_frameworks)]     //启动自定义测试框架
+#![test_runner(crate::test_runner)]     //将自定义测试框架的runner函数设为`crate::test_runner`
+/*
+    本来测试框架会自动生成一个main函数作为入口，既主函数。然后执行runner函数
+    但是这里使用了`#![no_main]`禁用了main函数，所以需要重新命名测试主函数，并在_start()函数中调用它
+*/
+#![reexport_test_harness_main = "test_main"]    //将自动生成的测试主函数命名为"test_main"
 
 mod vga_buffer;
 
@@ -12,6 +19,10 @@ pub extern "C" fn _start() -> ! {
     // 因为编译器会寻找一个名为`_start`的函数，所以这个函数就是入口点
     // 默认命名为`_start`
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+        test_main();
+
     loop {}
 }
 
@@ -20,4 +31,13 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
