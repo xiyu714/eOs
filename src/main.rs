f2304bcf2537ee51cb098a2436fdfc4750777e0d
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-11-13 13:00:42 +0800

    简单处理double fault

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,10 @@ pub extern "C" fn _start() -> ! {
 
     eOs::init(); // new
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3(); // new
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
+
 
     #[cfg(test)]
         test_main();
